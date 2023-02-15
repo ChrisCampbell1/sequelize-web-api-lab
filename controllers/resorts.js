cfddcb5cf8d3d0f0b1ec645c5c64eabf14d864f7
Chris Campbell <christopherecampbell@gmail.com>
2023-02-14 20:19:45 -0700

    stub up all routes and controllers

--- controllers/resorts.js
+++ controllers/resorts.js
@@ -1,6 +1,49 @@
 const { Resort } = require("../models")
 
+const create = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const index = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const show = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const deleteResort = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
 
 module.exports = {
-
+  create,
+  index,
+  show,
+  update,
+  delete: deleteResort
 }
--- routes/resorts.js
+++ routes/resorts.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const resortsCtrl = require('../controllers/resorts.js')
 
-
+router.post('/', resortsCtrl.create)
+router.get('/', resortsCtrl.index)
+router.get('/:id', resortsCtrl.show)
+router.put('/:id', resortsCtrl.update)
+router.delete('/', resortsCtrl.delete)
 
 module.exports = router
